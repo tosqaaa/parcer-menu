7d5e70ff8b7273a4c8485cd9097f33c16d2abfdd
tosqaaa <ilyarudakov.by@gmail.com>
2023-04-05 20:04:32 +0300

    Removed function get_menu() into another module

--- DatabaseHandler.py
+++ DatabaseHandler.py
@@ -1,5 +1,27 @@
 import sqlite3
-from main import get_menu
+import requests
+from bs4 import BeautifulSoup
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+MENU_LINK = os.getenv("MENU_LINK")
+
+def get_menu(LINK=MENU_LINK):
+    names = []
+    weight = []
+    price = []
+    response = requests.get(url=MENU_LINK)
+    soup = BeautifulSoup(response.text, "lxml")
+    table = soup.find('table', {'class': "table table-bordered"})
+    trs = table.find_all('tr')
+    for tr in trs:
+        tds = tr.find_all('td')
+        if len(tds) == 3:
+            names.append(tds[0].text)
+            weight.append(tds[1].text)
+            price.append(tds[2].text)
+    return (names[1:], weight[1:], price[1:])
 
 
 class DatabaseHandler:
--- main.py
+++ main.py
@@ -1,28 +1,5 @@
-import os
-import requests
-from bs4 import BeautifulSoup
-from dotenv import load_dotenv
 from DatabaseHandler import DatabaseHandler
 
-load_dotenv()
-MENU_LINK = os.getenv("MENU_LINK")
-
-
-def get_menu(LINK=MENU_LINK):
-    names = []
-    weight = []
-    price = []
-    response = requests.get(url=MENU_LINK)
-    soup = BeautifulSoup(response.text, "lxml")
-    table = soup.find('table', {'class': "table table-bordered"})
-    trs = table.find_all('tr')
-    for tr in trs:
-        tds = tr.find_all('td')
-        if len(tds) == 3:
-            names.append(tds[0].text)
-            weight.append(tds[1].text)
-            price.append(tds[2].text)
-    return (names[1:], weight[1:], price[1:])
 
 
 def main():
